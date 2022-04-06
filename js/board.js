9ce17e63e2c033e35fc7e8769057c2a0a7f143dd
Babacar <bndoyecs@gmail.com>
2022-04-06 04:50:29 -0400

    Tic Tac Toe Frontend View

---
+++ js/board.js
@@ -0,0 +1,50 @@
+/**
+ * ctx.moveTo(x,y) - Move the cursor to the position
+ * ctx.lineTo(x, y) - Start from that cursor position and end at this new position
+ * ctx.stroke() - Draw the line
+ *  
+ * The main reason I use values 200, 400 is I need to draw 4 lines (2 vertical/2 horizontal) in a perfect square (600 by 600).
+ * In the event, you want to make the square bigger it would be easier to keep it a perfect square.
+ * EX: 900 by 900, you could use values 300, 600 since that would divide it equally without decimals.
+ */
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const yMax = 600;
+const xMax = 600;
+
+canvas.width = yMax;
+canvas.height = xMax;
+
+canvas.style.left = "30%";
+canvas.style.top = "25%";
+canvas.style.position = "absolute";
+
+// Vertical Line One (from left to right)
+ctx.beginPath();
+ctx.strokeStyle = 'blue';
+ctx.moveTo(200, 0);
+ctx.lineTo(200, yMax);
+ctx.stroke();
+
+// Vertical Line Two (from left to right)
+ctx.beginPath();
+ctx.strokeStyle = 'green';
+ctx.moveTo(400, 0);
+ctx.lineTo(400, yMax);
+ctx.stroke();
+
+// Horizontal Line One (from top to bottom)
+ctx.beginPath();
+ctx.strokeStyle = 'red';
+ctx.moveTo(0, 200);
+ctx.lineTo(xMax, 200);
+ctx.stroke();
+
+// Horizontal Line Two (from top to bottom)
+ctx.beginPath();
+ctx.strokeStyle = 'orange';
+ctx.moveTo(0, 400);
+ctx.lineTo(xMax, 400);
+ctx.stroke();
